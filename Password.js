ff9921fb045ec75c8cc6857b17a842bf5a90ba4b
[prework-study-guide] <78627760+DollyA-bit@users.noreply.github.com>
2023-01-10 01:29:14 +0000

    created html file

---
+++ Password.js
@@ -0,0 +1,22 @@
+function generatePassword(maxLength = 64) {
+    const length = Math.floor(Math.random() * (maxLength - 10 + 1)) + 10;
+    let password = '';
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@%+\\/!#$^?:,)(}{][~-_.';
+  
+    for (let i = 0; i < length; i++) {
+      const character = characters[Math.floor(Math.random() * characters.length)];
+      password += character;
+    }
+  
+    return password;
+  }
+  const password = generatePassword();
+
+  let newPassword = generatePassword();
+console.log(newPassword); // Outputs a new random password
+  
+  
+  document.getElementById("generate-btn").addEventListener("click", function() {
+    let password = generatePassword();
+    document.getElementById("password-input").value = password;
+  });
